abd3e0a33798c21cdb03857043879e43165acf55
MUNYYB UR RYHMAN <hm.muneeb.u.r@gmail.com>
2023-02-06 10:01:56 +0500

    Added tailwind and daisyUI

---
+++ Firebase/Config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyASLtn7GNMxXRvq7VwK9vZWHhDCUjdK3dw",
+  authDomain: "aidan-torrence.firebaseapp.com",
+  projectId: "aidan-torrence",
+  storageBucket: "aidan-torrence.appspot.com",
+  messagingSenderId: "986725428375",
+  appId: "1:986725428375:web:ef03c180143ac2a04b8dd2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
